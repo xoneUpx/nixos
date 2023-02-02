bcc816ae75ba4a872cd72a7faf29eb6500c55044
xoneUpx <bobokuos@gmail.com>
2023-02-02 16:30:33 +0100

    home6

--- flake.nix
+++ flake.nix
@@ -12,15 +12,17 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
+      #pkgs = import nixpkgs {
+      #  inherit system;
+      #  config = { allowUnfree = true; };
+      #};
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations = {
         bobok = home-manager.lib.homeManagerConfiguration {
-          inherit system pkgs;
+          #inherit system pkgs;
+          inherit pkgs;
           modules = [ ./home.nix 
 	  {	home = { 
 			username = "bobok";
